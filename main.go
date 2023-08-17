10c4ff16b023b8ce03c55e031c0d2a79a8d949a2
kociumba <kociumba1@gmail.com>
2023-08-17 20:15:01 +0200

    fuck the default utf-8 character handling in cmd

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
+
+	"github.com/pkg/browser"
 )
 
 func main() {
@@ -170,7 +173,7 @@ func encryptor(raw_url string, encryption_hash_full int, encryption_key string) 
 
 	for _, num := range inted_url {
 		encrypted_url += strconv.Itoa(encryption_hash_full * num)
-		encrypted_url += "_"
+		encrypted_url += "_" // thi is 😬
 	}
 
 	// println(encrypted_url)
@@ -194,7 +197,7 @@ func decryptor(processed_url string, encryption_hash_full int, encryption_key st
 	encrypted_url := strings.TrimPrefix(processed_url, encryption_key+".dev/")
 	encrypted_url = strings.TrimSuffix(encrypted_url, "/")
 
-	encrypted_numbers := strings.Split(strings.Trim(encrypted_url, "_"), "_")
+	encrypted_numbers := strings.Split(strings.Trim(encrypted_url, "_"), "_") // this is also 😬
 
 	// println("encrypted_numbers: ", encrypted_numbers)
 
@@ -213,6 +216,28 @@ func decryptor(processed_url string, encryption_hash_full int, encryption_key st
 	println(decrypted_url)
 	println("this is your decrypted URL")
 	println("<====================>")
+
+	err := opener(decrypted_url)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func opener(decrypted_url string) error {
+
+	if !strings.HasPrefix(decrypted_url, "https://") {
+		decrypted_url = "https://" + decrypted_url
+	}
+
+	matched, _ := regexp.MatchString(`^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|\/|\/\/)?[A-z0-9_-]*?[:]?[A-z0-9_-]*?[@]?[A-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$`, decrypted_url)
+
+	if !matched {
+		return nil
+	}
+
+	browser.OpenURL(decrypted_url)
 
 	return nil
 }
